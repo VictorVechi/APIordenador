356e2a241ad87dba88946621fbdab15fc0115509
VictorVechi <victorhvechi@gmail.com>
2023-08-29 21:32:21 -0300

    adição das tratativas de erros

--- app.js
+++ app.js
@@ -1,17 +1,28 @@
 import manipularArray from "./utils/manipularArray.js"
-
+import chalk from "chalk"
 
 let array = []
-
+let entrada = ""
 // Loop para pegar o input do usuário até que ele digite "sair"
-while (true) {
+while (entrada !== "SAIR") {
     // Chama a função para pegar o input do usuário
-    manipularArray.getEntrada(array)
+    entrada = manipularArray.getEntrada()
+
+    // Verifica se o input é vazio
+    if(entrada === "") {
+        console.log(chalk.redBright("Comando inválido!"))
+        continue
+    } // Verifica se o input é um número
+    else if (!isNaN(entrada)) {
+        console.log(chalk.redBright("Números não são válidos!"))
+        continue
+    }
+    // Adiciona o input do usuário no array
+    array.push(entrada)
     // Verifica se o último elemento do array é "sair"
     if (array[array.length - 1] === "SAIR") {
         // Se for, remove o último elemento do array e sai do loop
         array.pop()
-        break
     }
 }
 // Chama a função para imprimir o array
